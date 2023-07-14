54d451c3d81b8b906d3191032a3716cfb72ba626
Giovanni Washel <giowashel@gmail.com>
2023-07-14 14:55:49 -0400

    attempted connecting login page to backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,32 @@
-import React, {useState} from "react";
+import React, {useRef, useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import "./LoginRegister.css";
 import { Navbar } from "../components/navbar";
 export const Login = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [user, setUser] = useState([]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const options = {
+      method: "GET",
+      headers: { 'Content-Type': 'application/json'}}
+    fetch("http://localhost:8080/login", options).then((res) => {console.log(res)});
+  };
+  const handleClick = () => {
+    return( <Link to ="/dashboard"></Link>);
+  }
   return (
     <div className="login-register-container">
       <div className="auth-form-container">
         <h1 className="top-h1">Login</h1>
-        <form className="forms">
+        <form className="forms" onSubmit={handleSubmit}>
           <input
             className="form-input"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             type="text"
             placeholder="Email"
             required
@@ -28,13 +39,10 @@ export const Login = () => {
             placeholder="Password"
             required
           />
-         <Link to="/dashboard">
-            <button className="login-button" type="submit">
+            <button type="submit" className="login-button" type="submit"> 
               <span>LOGIN</span>
             </button>
-        </Link>
         </form>
-        
         <div>{errorMessage}</div>
         <Link to="/signup">
           <button className="link-button">

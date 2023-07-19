a8b104921f9e280cbae7381f81ab10d00aee9db0
gio <gwashel@iu.edu>
2023-07-18 21:41:10 -0400

    Revert "attempted connecting login page to backend"
    This reverts commit 54d451c3d81b8b906d3191032a3716cfb72ba626.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,21 @@
-import React, {useRef, useEffect, useState} from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import "./LoginRegister.css";
 import { Navbar } from "../components/navbar";
 export const Login = () => {
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const [user, setUser] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const options = {
-      method: "GET",
-      headers: { 'Content-Type': 'application/json'}}
-    fetch("http://localhost:8080/login", options).then((res) => {console.log(res)});
-  };
-  const handleClick = () => {
-    return( <Link to ="/dashboard"></Link>);
-  }
   return (
     <div className="login-register-container">
       <div className="auth-form-container">
         <h1 className="top-h1">Login</h1>
-        <form className="forms" onSubmit={handleSubmit}>
+        <form className="forms">
           <input
             className="form-input"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             type="text"
             placeholder="Email"
             required
@@ -39,10 +28,13 @@ export const Login = () => {
             placeholder="Password"
             required
           />
-            <button type="submit" className="login-button" type="submit"> 
+         <Link to="/dashboard">
+            <button className="login-button" type="submit">
               <span>LOGIN</span>
             </button>
+        </Link>
         </form>
+        
         <div>{errorMessage}</div>
         <Link to="/signup">
           <button className="link-button">

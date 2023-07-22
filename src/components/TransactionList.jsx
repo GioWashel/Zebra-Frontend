4943c69b4cf7337d933081bed8de5b16439e16bb
gio <gwashel@iu.edu>
2023-07-22 12:26:55 -0400

    added an input field that can filter transactions

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,33 +1,42 @@
 import { useState } from "react";
 import "./TransactionList.css";
 import data from "../data/Data.json";
+
+
 export const TransactionList =  (props) => {
+
+    const [transactions, setTransactions] = useState(data);
+    const [filter, setFilter] = useState("");
+
+
 
     return(
         <div className="transaction-container">
+            <input onChange={(e) => setFilter(e.target.value)} id="input" placeholder="Transaction" type="text"/>
             <table width="1000" className="transaction-list">
-            <tr>
-                <th width="200">Select</th>
-                <th width="200">Date</th>
-                <th width="200">Description</th>
-                <th width="200">Amount</th>
-                <th width="200">Category </th>
-            </tr>
-            {data.map((transaction) => {
-                return( 
                 <tr>
-                    <td id="checkbox"><input type="checkbox"/></td>
-                    <td id="date">{transaction.date}</td>
-                    <td id="desc">{transaction.description}</td>
-                    <td id="amount">{transaction.amount}</td>
-                    <td id="category"><button>{transaction.category}</button></td>
-                </tr>);
-                })
-            }
+                    <th width="200">Select</th>
+                    <th width="200">Date</th>
+                    <th width="200">Description</th>
+                    <th width="200">Amount</th>
+                    <th width="200">Category </th>
+                </tr>
+                {transactions.map((transaction) => {
+                    if(transaction.description.includes(filter) ||
+                       transaction.amount.includes(filter) ||
+                       transaction.date.includes(filter) ||
+                        transaction.category.includes(filter)) {
+                        return( 
+                            <tr>
+                                <td id="checkbox"><input type="checkbox"/></td>
+                                <td id="date">{transaction.date}</td>
+                                <td id="desc">{transaction.description}</td>
+                                <td id="amount">{transaction.amount}</td>
+                                <td id="category"><button>{transaction.category}</button></td>
+                            </tr>); 
+                    }})}
+                    
             </table>
         </div>
-        
-
-    );
-    
+    ); 
 }
